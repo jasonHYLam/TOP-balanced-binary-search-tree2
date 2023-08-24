a0470c407336c14baf8dee1680d7e10c9a4a97d7
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-24 14:08:51 +0100

    Create Driver Script To Test Tree Functions
    Created a tree, checked if it was balanced, unbalanced it, checked if it
    was unbalanced, then rebalanced the tree, then printed the trees in
    various traversal orders.

--- src/index.js
+++ src/index.js
@@ -24,97 +24,38 @@ function unbalanceTree(tree) {
     }
 }
 
+function printTreeOrders(tree) {
+    console.log(
+        'level order',
+        tree.levelOrderIterative(),
+        'pre order',
+        tree.preOrder(),
+        'post order',
+        tree.postOrder(),
+        'in order',
+        tree.inOrder()
+    )
+}
 
-// console.log(testArray)
 
-// buildTree(testArray)
-// let newTree = new Tree(testArray)
 let newTree = new Tree(createRandomArray())
 console.log(
     newTree.isBalanced()
 )
 
-console.log(
-    'level order',
-    newTree.levelOrderIterative()
-)
-console.log(
-    'pre order',
-    newTree.preOrder()
-)
-console.log(
-    'post order',
-    newTree.postOrder()
-)
-console.log(
-    'in order',
-    newTree.inOrder()
-)
 
 unbalanceTree(newTree)
 
-// newTree.insert(9)
+printTreeOrders(newTree)
+
 prettyPrint(newTree.root)
+
+console.log(newTree.isBalanced())
 
 newTree.rebalance()
 
-console.log(
-    newTree.isBalanced()
-)
+console.log(newTree.isBalanced())
 
 prettyPrint(newTree.root)
-// newTree.delete(323)
-// prettyPrint(newTree.root)
 
-newTree.find(6)
-newTree.find(1)
-newTree.find(5)
-
-// newTree.levelOrderIterative((node) => console.log(node))
-
-newTree.levelOrderIterative();
-newTree.preOrder()
-
-// newTree.preOrder((node)=> console.log(node.data))
-
-// newTree.inOrder((node)=>console.log(node.data))
-
-newTree.postOrder()
-
-// console.log(
-//     newTree.h,eight(50)
-// )
-
-// console.log(
-//     newTree.height(6345)
-// )
-
-
-
-// console.log(
-//     newTree.isBalanced()
-// )
-
-// newTree.insert(999)
-// newTree.insert(322)
-// newTree.insert(321)
-// newTree.insert(320)
-// newTree.insert(0)
-// prettyPrint(newTree.root)
-// console.log(
-//     newTree.height(32477)
-// )
-// console.log(
-//     newTree.depth(320)
-// )
-// console.log(
-//     newTree.height(5)
-// )
-// console.log(
-//     newTree.isBalanced2()
-// )
-// console.log(
-//     newTree.isBalanced()
-// )
-
-// newTree.rebalance();
+printTreeOrders(newTree)
